c5210add9171d16dd696cbce6d50b096efbcbdaf
L4ZZA <pietro.lazz@gmail.com>
2020-10-29 10:49:07 +0100

    Fixed compilation

--- hello/go.mod
+++ hello/go.mod
@@ -3,5 +3,9 @@ module hello
 go 1.15
 
 replace example.com/handlers => ../handlers
+replace example.com/data => ../data
 
-require example.com/handlers v0.0.0-00010101000000-000000000000 // indirect
+require (
+    example.com/handlers v0.0.0-00010101000000-000000000000
+    example.com/data v0.0.0-00010101000000-000000000000
+)
--- hello/hello.go
+++ hello/hello.go
@@ -1,31 +1,57 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"example.com/handlers"
 )
 
 func main() {
-	// Set properties of the predefined Logger, including
-	// the log entry prefix and a flag to disable printing
-	// the time, source file, and line number.
-	log.SetPrefix("handlers: ")
-	log.SetFlags(0)
+	l := log.New(os.Stdout, "products-api ", log.LstdFlags)
 
-	// A slice of names.
-	names := []string{"Gladys", "Samantha", "Darrin"}
+	// create the handlers
+	ph := handlers.NewProducts(l)
 
-	// Request a greeting message.
-	message, err := handlers.Hellos(names)
-	// If an error was returned, print it to the console and
-	// exit the program.
-	if err != nil {
-		log.Fatal(err)
+	// create a new serve mux and register the handlers
+	sm := http.NewServeMux()
+	sm.Handle("/", ph)
+
+	// create a new server
+	s := http.Server{
+		Addr:         ":9090",           // configure the bind address
+		Handler:      sm,                // set the default handler
+		ErrorLog:     l,                 // set the logger for the server
+		ReadTimeout:  5 * time.Second,   // max time to read request from the client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
 	}
 
-	// If no error was returned, print the returned message
-	// to the console.
-	fmt.Println(message)
+	// start the server
+	go func() {
+		l.Println("Starting server on port 9090")
+
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	// trap sigterm or interupt and gracefully shutdown the server
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	// Block until a signal is received.
+	sig := <-c
+	log.Println("Got signal:", sig)
+
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ctx)
 }
